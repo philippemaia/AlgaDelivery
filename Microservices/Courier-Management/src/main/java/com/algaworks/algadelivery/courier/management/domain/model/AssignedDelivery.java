2e9b328cb10cc7f7fd3de13792c798414a7bd5ee
Philippe Maia <philippe.m.maia@hotmail.com>
2026-01-14 00:09:15 -0300

    Incluindo persistência para o microserviço de Courier

--- Microservices/Courier-Management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/AssignedDelivery.java
+++ Microservices/Courier-Management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/AssignedDelivery.java
@@ -1,25 +1,33 @@
 package com.algaworks.algadelivery.courier.management.domain.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class AssignedDelivery {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
 
     private OffsetDateTime assignedAt;
 
-    static AssignedDelivery pending(UUID deliveryId) {
-        AssignedDelivery assignedDelivery = new AssignedDelivery();
-        assignedDelivery.setId(deliveryId);
-        assignedDelivery.setAssignedAt(OffsetDateTime.now());
-        return assignedDelivery;
+    @ManyToOne(optional = false)
+    @Getter(AccessLevel.PRIVATE)
+    private Courier courier;
+
+    static AssignedDelivery pending(UUID deliveryId,Courier courier) {
+        AssignedDelivery delivery = new AssignedDelivery();
+        delivery.setId(deliveryId);
+        delivery.setAssignedAt(OffsetDateTime.now());
+        delivery.setCourier(courier);
+        return delivery;
     }
 }
