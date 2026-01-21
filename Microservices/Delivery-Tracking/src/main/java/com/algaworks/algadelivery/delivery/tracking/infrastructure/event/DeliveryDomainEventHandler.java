aea9082635b43a4b2f17f105541e696d654124bb
Philippe Maia <philippe.m.maia@hotmail.com>
2026-01-20 22:07:09 -0300

    Implementando o Produtor

--- Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
@@ -3,28 +3,36 @@ package com.algaworks.algadelivery.delivery.tracking.infrastructure.event;
 import com.algaworks.algadelivery.delivery.tracking.domain.event.DeliveryFulfilledEvent;
 import com.algaworks.algadelivery.delivery.tracking.domain.event.DeliveryPickUpEvent;
 import com.algaworks.algadelivery.delivery.tracking.domain.event.DeliveryPlacedEvent;
+import com.algaworks.algadelivery.delivery.tracking.infrastructure.kafka.KafkaTopicConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import static com.algaworks.algadelivery.delivery.tracking.infrastructure.kafka.KafkaTopicConfig.deliveryEventsTopicName;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class DeliveryDomainEventHandler {
 
+    private final IntegrationEventPublisher integrationEventPublisher;
+
     @EventListener
     public void handle(DeliveryPlacedEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), deliveryEventsTopicName);
     }
 
     @EventListener
     public void handle(DeliveryPickUpEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), deliveryEventsTopicName);
     }
 
     @EventListener
     public void handle(DeliveryFulfilledEvent event) {
         log.info(event.toString());
+        integrationEventPublisher.publish(event, event.getDeliveryId().toString(), deliveryEventsTopicName);
     }
 }
